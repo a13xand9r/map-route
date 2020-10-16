020b6128d06645ffc361ea34acc5e6bb5b668b71
Aleksandr <a13x.student@yandex.ru>
2020-10-16 12:05:17 +0300

    Updates

---
+++ precache-manifest.33802c466a3941f84027bc58bce8f8f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a98d8aaa8e0f783f71535bc98e160806",
+    "url": "/map-route/index.html"
+  },
+  {
+    "revision": "f4a6592f08c6f43ea893",
+    "url": "/map-route/static/css/main.db75f813.chunk.css"
+  },
+  {
+    "revision": "74c4dbd68e638eb392aa",
+    "url": "/map-route/static/js/2.0dab69f2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/map-route/static/js/2.0dab69f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4a6592f08c6f43ea893",
+    "url": "/map-route/static/js/main.04c43e7c.chunk.js"
+  },
+  {
+    "revision": "1a89d6d41691aaed0a8c",
+    "url": "/map-route/static/js/runtime-main.466fba75.js"
+  }
+]);
