0a2bfa9380fe8eacd8e2a1b43470d9b5340b4079
Aleksandr <a13x.student@yandex.ru>
2020-11-04 15:22:15 +0300

    tests updated

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -10,28 +10,3 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div)
 })
 
-
-let browser: Browser
-let page: Page
-beforeAll(async () => {
-  browser = await puppeteer.launch({})
-  page = await browser.newPage()
-  await page.goto('http://localhost:3000/')
-})
-afterAll(() => {
-  browser.close()
-})
-
-test('point should be added', async () => {
-  await page.click('input#newPointInput')
-  await page.type('input#newPointInput', 'Paris')
-  await page.click('input#newPointInput')
-  await page.keyboard.press('Enter')
-  await page.waitForResponse(() => true)
-
-  const pointsList = await page.$$('.routePoints_routeItem__3p26C')
-  const point = await page.$eval('.routePoints_routeItem__name__2IrVq', e => e.innerHTML)
-  expect(pointsList.length).toBe(1)
-  expect(point).toBe('Paris')
-})
-
